15df795bf77c5679e7e9228b658acd0d81ffcb10
macneko-ayu <macneko.ayu@gmail.com>
2021-07-25 01:33:35 +0900

    Implement textlint-server

---
+++ textlint-server/src/routes/v1/textlint.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import { TextLintEngine } from 'textlint';
+
+const router = express.Router();
+
+router.post('/', async (req: express.Request, res: express.Response) => {
+    try {
+        const body = req.body.text;
+        const engine = new TextLintEngine();
+        const results = await engine.executeOnText(body);
+        const messages = results[0].messages.map(item => item.message);
+        res.status(200).json({ messages: messages });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+export default router;
