c2864ae6c0c8902a0226ec2827a2e42b4a29a944
macneko-ayu <macneko.ayu@gmail.com>
2021-07-27 22:30:58 +0900

    Use cors package

--- textlint-server/src/routes/v1/textlint.ts
+++ textlint-server/src/routes/v1/textlint.ts
@@ -1,17 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import { TextLintEngine } from 'textlint';
 
 const router = express.Router();
 
-// CORS
-router.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "*");
-    res.header("Access-Control-Allow-Headers", "*");
-    next();
-})
-
-router.post('/', async (req: express.Request, res: express.Response) => {
+router.post('/', cors(), async (req: express.Request, res: express.Response) => {
     try {
         const body = req.body.text;
         const engine = new TextLintEngine();
