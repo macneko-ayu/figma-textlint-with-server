0bf48495eaeb1499c68d44f4f3eb4f2d6ce1358d
macneko-ayu <macneko.ayu@gmail.com>
2021-08-05 23:32:13 +0900

    Change the plugin to pass values in an array.

--- figma-textlint/code.ts
+++ figma-textlint/code.ts
@@ -3,11 +3,14 @@ figma.showUI(__html__);
 figma.ui.onmessage = msg => {
   if (msg.type === 'execute') {
     const textNodes = figma.currentPage.findAll(node => node.type === 'TEXT');
-    textNodes.forEach(node => {
-      const characters = (node as TextNode).characters;
-      const nodeId = node.id;
-      figma.ui.postMessage({ type: 'send-text', fileKey: figma.fileKey, nodeId: nodeId, characters: characters })
-    })
+    const textInfos = textNodes.map(node => {
+      return {
+        fileKey: figma.fileKey,
+        characters: (node as TextNode).characters,
+        nodeId: node.id
+      };
+    });
+    figma.ui.postMessage({ type: 'send-text', textInfos })
   }
 
   if (msg.type === 'complete') {
